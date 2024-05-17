30152f30a4bc3f2a7d7b750402db428fd0f525a2
zaxtraa <422000663@ntc.edu.ph>
2024-05-17 23:37:48 +0800

    Add files via upload

---
+++ myToken.sol
@@ -0,0 +1,28 @@
+
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "AZURE";
+    string public tokenAbbrv = "AZ";
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint (address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    // burn function
+    function burn (address _address, uint _value) public {
+        if (balances[_address] >= _value) {
+            totalSupply -= _value;
+            balances[_address] -= _value;
+        }
+    }
+
+}
